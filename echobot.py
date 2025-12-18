4bd9cc466c1e44555cb0da9c0930f0d22e5d7879
BonnieZhangNC <116578742+BonnieZhangNC@users.noreply.github.com>
2025-12-18 17:38:06 -0500

    Update greeting and add bar chart in echobot
    Change user greeting to assistant and add a bar chart.

--- echobot.py
+++ echobot.py
@@ -1,4 +1,6 @@
-import streamlit as st
+Chris Santo 12/18/2025 5:35 PM • import streamlit as st
+import numpy as np
 
-with st.chat_message("user"):
-  st.write("Hello 👋")
+with st.chat_message("assistant"):
+  st.write("Hello human")
+  st.bar_chart(np.random.randn(30, 3))
